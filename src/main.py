32030e8a33ddebc2d4cc9321d47feb48235d442f
Nicholas Chmielewski <nicholas.chmielewski.j@gmail.com>
2023-04-06 16:42:37 -0700

    w

--- src/main.py
+++ src/main.py
@@ -8,72 +8,77 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+class Bot():
+    
+    def __init__(self):
+        self.c = discord.Client()
 
-@client.event
-async def on_ready():
-    print(f'{client.user.name} has connected to Discord!')
+    client = discord.Client()
 
-@client.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(
-        f'Hi {member.name}, welcome to my Discord server!'
-    )
+    @client.event
+    async def on_ready(self):
+        print(f'{self.client.user.name} has connected to Discord!')
 
-# A Default event
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+    @client.event
+    async def on_member_join(member):
+        await member.create_dm()
+        await member.dm_channel.send(
+            f'Hi {member.name}, welcome to my Discord server!'
+        )
 
-    if message.content == '99!':
-        await message.channel.send('test')
+    # A Default event
+    @client.event
+    async def on_message(self, message):
+        if message.author == self.client.user:
+            return
 
-# Play the classic game of Rock, Paper, Scissors
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+        if message.content == '99!':
+            await message.channel.send('test')
 
-    if message.content == '!rps':
-        player = message.author
-        await message.channel.send("Let's play rock paper scissors.  The computer will randomly pick an option.", '\n')
-        game = rps.RockPaperScissors()
+    # Play the classic game of Rock, Paper, Scissors
+    @client.event
+    async def on_message(self, message):
+        if message.author == self.client.user:
+            return
 
-        while True:
-            
-            com = random.choice(['Rock', 'Paper', 'Scissors'])
-            
-            if message.author != player:
-                continue
-            
-            i = message.content.game.convert(i)
+        if message.content == '!rps':
+            player = message.author
+            await message.channel.send("Let's play rock paper scissors.  The computer will randomly pick an option.", '\n')
+            game = rps.RockPaperScissors()
 
-            if i == 'q':
-                await message.channel.send('Thanks for playing!')
-                break
-            elif i == 'e':
-                await message.channel.send('Not valid option!  Please enter r/R/rock/Rock or p/P/paper/Paper or s/S/scissors/Scissors')
-                continue
+            while True:
+                
+                com = random.choice(['Rock', 'Paper', 'Scissors'])
+                
+                if message.author != player:
+                    continue
+                
+                i = message.content.game.convert(i)
 
-            v = game.verdict(i, com)
+                if i == 'q':
+                    await message.channel.send('Thanks for playing!')
+                    break
+                elif i == 'e':
+                    await message.channel.send('Not valid option!  Please enter r/R/rock/Rock or p/P/paper/Paper or s/S/scissors/Scissors')
+                    continue
 
-            match v:
-                case 0:
-                    await message.channel.send(f'{i} beats {com} - Player wins!')
-                    game.score[0] += 1
-                    
-                case 1:
-                    await message.channel.send(f'{com} beats {i} - Computer wins!')
-                    game.score[1] += 1
-                    
-                case 2:
-                    await message.channel.send(f'Both picked {i} - Tie!')
-                    game.score[2] += 1    
-            
-            await message.channel.send(f'Player won {game.score[0]}   Computer won {game.score[1]}   {game.score[2]} Ties', '\n')
-            await message.channel.send("Enter your choice: ")
+                v = game.verdict(i, com)
+
+                match v:
+                    case 0:
+                        await message.channel.send(f'{i} beats {com} - Player wins!')
+                        game.score[0] += 1
+                        
+                    case 1:
+                        await message.channel.send(f'{com} beats {i} - Computer wins!')
+                        game.score[1] += 1
+                        
+                    case 2:
+                        await message.channel.send(f'Both picked {i} - Tie!')
+                        game.score[2] += 1    
+                
+                await message.channel.send(f'Player won {game.score[0]}   Computer won {game.score[1]}   {game.score[2]} Ties', '\n')
+                await message.channel.send("Enter your choice: ")
 
 
-client.run(TOKEN)
+    client.run(TOKEN)
