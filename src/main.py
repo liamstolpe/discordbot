004c1ced1fa7f935bd46d9fa180ea9226b9f8d2d
liamstolpe <116044261+liamstolpe@users.noreply.github.com>
2023-04-07 00:07:35 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import discord
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv('OTAyOTE2NTcwOTY2Mjc0MDU4.Gk2rzB.ym44fF9diVveVASWSyYupvfmKKJMnMvCN2IQIA')
 
 client = discord.Client()
 
